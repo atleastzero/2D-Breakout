36bcaf21e0559c5d6c3733be2f63203903ca79ef
atleastzero <megan.obryan52@gmail.com>
2020-11-04 15:31:54 -0800

    Added Sprite class

---
+++ Sprite.js
@@ -0,0 +1,18 @@
+class Sprite {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  moveTo(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  moveBy(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  }
+}
+
+export default Sprite;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  *  Constants
  *  ======================================================== */
 
-// Canvas
+// DOM References
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
