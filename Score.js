8828be86dca9ee556f1a8d85dbce429b39b995f1
atleastzero <megan.obryan52@gmail.com>
2020-11-04 15:59:50 -0800

    Added Score class.

---
+++ Score.js
@@ -0,0 +1,20 @@
+import ctx, { canvas } from './main';
+
+import Sprite from './Sprite';
+
+class Score extends Sprite {
+  constructor(font, color) {
+    super(canvas.width - 65, 20);
+    this.font = font;
+    this.color = color;
+    this.total = 0;
+  }
+
+  render() {
+    ctx.font = this.font;
+    ctx.fillStyle = this.color;
+    ctx.fillText(`Score: ${this.total}`, 8, 20);
+  }
+}
+
+export default Score;
